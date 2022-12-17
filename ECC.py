4db73b64dfd318bc2fc255685aaec2f1c617ffd2
Matthew Salinger <42982273+mattsal13@users.noreply.github.com>
2022-12-17 23:34:00 +0000

    EC arithmetic

--- ECC.py
+++ ECC.py
@@ -1,15 +1,13 @@
-
-
 ## The group operations. P and Q are points in the plane.
 def ECA_real(P, Q, A):
     x_1, y_1 = P[0], P[1]
     x_2, y_2 = Q[0], Q[1]
-    # The case where x1 != x2.
+    # The case where x1 != x2
     try:
         m = (y_2 - y_1)/(x_2 - x_1)
         x_3 = m**2 - x_1 - x_2
-        y_3 = m*(x_1-x_2) - y_1
-    # Handles the case where x1 = x2.
+        y_3 = m*(x_1 - x_2) - y_1
+    # Handles the case where x1 = x2
     except ZeroDivisionError:
         if y_1 != y_2:
             return 'inf'
@@ -20,12 +18,10 @@ def ECA_real(P, Q, A):
                 y_3 = m_t*(x_1 - x_3) - y_1
             else:
                 return 'inf'
-
     return x_3, y_3
 
+## Associativity is hard to prove. But here are some examples.
+# B, C, D = (0,4), (2,-5), (4, 9)
 
-# P = [1,4]
-# Q = [3,10]
-# A = -2
-
-print(ECA_real([1,4], [3,10], 2))
+# print(ECA_real(ECA_real(B, C, -2), D, -2))
+# print(ECA_real(B, ECA_real(C, D, -2), -2))
