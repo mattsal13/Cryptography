bf1319b5ed615f964839b3307ffe2aeea3639e45
Matthew Salinger <42982273+mattsal13@users.noreply.github.com>
2023-01-13 18:29:28 -0500

    1/13 update

--- XOR.py
+++ XOR.py
@@ -44,8 +44,13 @@ def XOR(byte1, byte2):
 
 # XOR on bit STRINGS. Returns a STRING.
 def XORs(byte1, byte2):
+    # Need to reverse the bytes so XOR goes from least to greatest, then reverse back.
+    byte1 = byte1[::-1]
+    byte2 = byte2[::-1]
+    # The result of XOR will always be <= in length to the max. 
     m = max(len(byte1), len(byte2))
     new_byte = []
+    # Operation
     for i in range(m):
         try:
             new_byte.append((int(byte1[i]) + int(byte2[i])) % 2)
@@ -55,15 +60,26 @@ def XORs(byte1, byte2):
             except IndexError:
                 new_byte.append(int(byte2[i]))
     
-    return new_byte
-
-example = XOR(bitwise(8), bitwise(25))
+    # Reverse back.
+    new_byte = new_byte[::-1]
+    # Note: new_byte may have leading zeros! This will eliminate leading zeros until
+    # either a 1 is detected or we hit the very last bit, in which case we just return 0.
+    counter = 0
+    zero_bit = True
+    L = len(new_byte) 
+    while counter <= L - 2 and zero_bit:
+        if new_byte[0] == 1:
+            zero_bit = False
+        else:
+            new_byte.pop(0)
+            counter += 1
+    # Finally return a string.
+    new_byte = [str(x) for x in new_byte]
+    return ''.join(new_byte)
 
 ## Now define XOR arithmetic.
 def XORa(x, y):
     return decimal(XOR(bitwise(x), bitwise(y)))
 
-# Should return 17.
-# print(XORa(8, 25))
-# print(XORa(45, 45))
-# print(XORa(8, 16))
+print(XORs('100101', '101010111'))
+print(XORs('100101011', '101010111'))
