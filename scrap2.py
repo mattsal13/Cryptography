d7dfdb58460d68ee7bd4c1be40fdecd4057a99a3
Matthew Salinger <42982273+mattsal13@users.noreply.github.com>
2023-01-14 00:51:52 -0500

    Final update 1/13

--- scrap2.py
+++ scrap2.py
@@ -6,7 +6,7 @@ CT = '6c73d5240a948c86981bc294814d'
 PT_ascii = [ord(char) for char in PT]
 PT_ascii_bin = ["{0:08b}".format(int) for int in PT_ascii]
 
-CT_chrs = [CT[i] + CT[i + 1] for i in range(len(CT) // 2)]
+CT_chrs = [CT[i] + CT[i + 1] for i in range(0, len(CT), 2)]
 CT_chrs_bin = ["{0:08b}".format(int(chr, 16)) for chr in CT_chrs]
 
 # print(PT_ascii)
@@ -17,7 +17,7 @@ CT_chrs_bin = ["{0:08b}".format(int(chr, 16)) for chr in CT_chrs]
 
 ## Now we XOR the PT and CT together to get the key. 
 
-key_chrs = [XORs(x, y) for (x,y) in zip(PT_ascii_bin, CT_chrs_bin)]
+key_chrs = [XORs(x, y) for (x, y) in zip(PT_ascii_bin, CT_chrs_bin)]
 
 print()
 print(f'Key: {key_chrs}')
@@ -27,12 +27,14 @@ print()
 
 PT2 = 'attack at dusk'
 PT2_ascii = [ord(chr) for chr in PT2]
-PT2_bin = ["{0:08b}".format(int) for int in PT2_ascii]
+PT2_bin = ["{0:08b}".format(x) for x in PT2_ascii]
 
 CT2_bin = [XORs(x, y) for (x, y) in zip(key_chrs, PT2_bin)]
-CT2_hex = [hex(int(byte, 2)).lstrip('0x') for byte in CT2_bin]
+CT2_hex = [hex(int(byte, 2)).lstrip('0x').zfill(2) for byte in CT2_bin]
 
 ## These should only differ in the last 3 bytes (ie last 6 hex digits)
 print('Attack at dawn: ' + '6c73d5240a948c86981bc294814d')
 print()
 print('Attack at dusk: ' + ''.join(CT2_hex))
+
+# This is correct.
